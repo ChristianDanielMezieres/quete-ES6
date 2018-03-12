6f5dd240b81c8e42f3e165ac2fcc0d0122cb0046
Chrys <34030305+ChristianDanielMezieres@users.noreply.github.com>
2018-03-12 16:18:21 +0100

    Add files via upload

---
+++ quete-ES6.ts
@@ -0,0 +1,12 @@
+
+
+
+let town:string = "Lèves"
+let age:number = 48
+const birthday:string = " 8 février 1970 "
+
+let sentence = `I live in ${town}, I am ${age} years old and I am born in ${birthday}` 
+
+console.log(sentence);
+
+
